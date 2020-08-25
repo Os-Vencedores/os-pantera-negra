e00db55adb9dcc427a502fd552021a23e39b021f
Vailton Gomes <governador.adm@gmail.com>
2020-08-25 14:08:53 -0300

    Vailton1

--- Site_Toters_Sistema/Site_Toters_Sistema/Models/toter.cs
+++ Site_Toters_Sistema/Site_Toters_Sistema/Models/toter.cs
@@ -12,15 +12,15 @@ namespace Site_Toters_Sistema.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         [StringLength(60, MinimumLength = 6)]
         public string NomeCompleto { get; set; }
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         [StringLength(20, MinimumLength = 4)]
         public string Nacionalidade { get; set; }
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         [StringLength(30)]
         [Display(Name = "Cidade")]
         public string Cidade { get; set; }
@@ -29,13 +29,13 @@ namespace Site_Toters_Sistema.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         [DataType(DataType.PhoneNumber, ErrorMessage = "(00) 00000-0000")]
         [DisplayName("Telefone")]
         public string Telefone { get; set; }
 
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
